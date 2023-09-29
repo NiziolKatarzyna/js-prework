a999282053ea0c1e9dd5242b9e527ded3cdd77bd
Katarzyna Nizioł <niziol.katarzyna25@gmail.com>
2023-09-29 22:34:48 +0200

    Add if and else for playerMove

--- js/functions.js
+++ js/functions.js
@@ -16,6 +16,21 @@ if (randomNumber == '1') {
 }
 printMessage('Mój ruch: ' + computerMove);
 
+var item, playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == ' 2') {
+  playerMove = ' papier';
+} else if (playerInput == '3') {
+  playerMove = ' nożyce';
+} else {
+  printMessage(' Błędny wybór');
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
+
 function clearMessages() {
   document.getElementById('messages').innerHTML = '';
 }
